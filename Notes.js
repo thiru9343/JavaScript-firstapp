df00fda55ee3b9aac07348f0b8bc64895af250e0
Thirupathi Kola <thirupathikola@Thirupathis-MacBook-Pro.local>
2020-04-27 17:56:09 -0400

    first commit

---
+++ Notes.js
@@ -0,0 +1,38 @@
+JavaScript
+
+JavaScript- JS is a logical part of front-end.
+
+MERN Stack 			MEAN Stack
+
+Mongo- It is a database and JavaScript based
+Express- It is a framework of NodeJS, and JavaScript based
+ReactJS/Angular- Frontend framework and build on JS
+Node- It is a backend server and it is built on JS
+
+
+ES5
+ES6
+ES7
+•	ECMAScript is a version of a base of JavaScript
+•	It is a standard
+•	It’s a base lib
+
+JS vs TypeScript
+o	TypeScript is a superset of JS.
+
+Variables- String, number, Boolean
+
+
+ES5- var
+ES6- let, const
+
+var = we can redeclare and reassign 
+let = we cannot redeclare, but we can reassign
+const = we cannot redeclare nor reassign
+
+
+How we can write JS code and execute:
+1.	Any browser- right click and inspect
+2.	Open terminal and type ‘node’(install node before you do) then, write&excute
+
+
