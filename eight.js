d189157272b9e6bfda33bc4e674603fd0e2d5f29
Thirupathi Kola <thirupathikola@Thirupathis-MacBook-Pro.local>
2020-04-28 00:10:31 -0400

    b

---
+++ eight.js
@@ -0,0 +1,29 @@
+var name="john"
+
+switch(name){
+    case 'aakash':
+        console.log('You are user')
+        break;
+    case 'john':
+        console.log('You are Admin')
+        break;
+    default:
+        console.log("I Dont know")
+}
+
+"You are user"
+
+
+switch(mydate.getDay()){
+    case 1:
+        console.log('Its Monday')
+        break;
+    case 2:
+        console.log('Its Tuesday')
+        break;
+    case 3:
+        console.log('Its Wednesday')
+        break;
+    default:
+        console.log('Wrong Input')
+}
---
+++ seven.js
@@ -0,0 +1,15 @@
+Date()
+"Wed Apr 22 2020 04:35:34 GMT+0100 (British Summer Time)"
+Date().getMonth()
+VM244:1 Uncaught TypeError: Date(...).getMonth is not a function
+    at <anonymous>:1:8
+(anonymous) @ VM244:1
+var mydate = new Date()
+mydate.getDate()
+22
+mydate.getMonth()
+3
+mydate.getFullYear()
+2020
+mydate.getDay()
+3
